2ae51d29bd7f7c3b0630dce746e358ef6a32ade8
Eric Moran <emoran0403@gmail.com>
2022-03-02 13:52:24 -0500

    updated navbar to not include exact links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,16 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="d-flex justify-content-center mt-3">
-      <Link exact to="/" className="btn btn-primary nav-link mx-2 text-light">
+      <Link to="/" className="btn btn-primary nav-link mx-2 text-light">
         Home
       </Link>
-      <Link exact to="/films" className="btn btn-primary nav-link mx-2 text-light">
+      <Link to="/films" className="btn btn-primary nav-link mx-2 text-light">
         Films
       </Link>
-      <Link exact to="/characters" className="btn btn-primary nav-link mx-2 text-light">
+      <Link to="/characters" className="btn btn-primary nav-link mx-2 text-light">
         Characters
       </Link>
-      <Link exact to="/locations" className="btn btn-primary nav-link mx-2 text-light">
+      <Link to="/locations" className="btn btn-primary nav-link mx-2 text-light">
         Locations
       </Link>
     </nav>
